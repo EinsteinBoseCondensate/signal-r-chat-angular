477f834cd309b0860be1b6489bcf319095a7a6ee
EinsteinBoseCondensate <antonio.miralles.perez.1@gmail.com>
2020-06-08 18:09:30 +0200

    height-fixed-in-chats-tab--friends-tab-still-pending

--- src/app/modules/chat/main/main.component.ts
+++ src/app/modules/chat/main/main.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+  ngAfterViewInit(): void {
+    document.querySelector(".mat-tab-body-wrapper").setAttribute("style", "height:100%;");
+    console.log(`AfterViewInit: ${document.querySelector(".mat-tab-body-wrapper")}`);
+    console.log(document.querySelector(".mat-tab-body-wrapper"));
+  }
 
   ngOnInit(): void {
+    document.querySelector(".mat-tab-body-wrapper").setAttribute('style', 'height:100%;');
+    console.log(`OnInit: ${document.querySelector(".mat-tab-body-wrapper")}`);
+    console.log(document.querySelector(".mat-tab-body-wrapper"));
   }
 
 }
